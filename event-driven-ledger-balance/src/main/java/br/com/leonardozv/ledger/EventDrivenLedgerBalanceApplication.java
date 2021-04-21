49c0ad4420b4b59c3da0da2a017476ff93a329bb
Leonardo Z Vasconcellos <leozvasconcellos@gmail.com>
2021-04-21 20:16:50 -0300

    Versão inicial

---
+++ event-driven-ledger-balance/src/main/java/br/com/leonardozv/ledger/EventDrivenLedgerBalanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardozv.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenLedgerBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenLedgerBalanceApplication.class, args);
+	}
+
+}
