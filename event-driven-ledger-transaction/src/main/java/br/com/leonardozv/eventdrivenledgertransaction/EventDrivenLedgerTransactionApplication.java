d98df9be26a1ae0fc2a6beee9b502767e5d83fba
Leonardo Z Vasconcellos <leozvasconcellos@gmail.com>
2021-04-21 21:11:37 -0300

    Criação projeto transaction

---
+++ event-driven-ledger-transaction/src/main/java/br/com/leonardozv/eventdrivenledgertransaction/EventDrivenLedgerTransactionApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardozv.eventdrivenledgertransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenLedgerTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenLedgerTransactionApplication.class, args);
+	}
+
+}
---
+++ event-driven-ledger-transaction/src/test/java/br/com/leonardozv/eventdrivenledgertransaction/EventDrivenLedgerTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardozv.eventdrivenledgertransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenLedgerTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
